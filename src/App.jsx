5cd752ee4d1c0d3ddd62ade7ed41e1f05019cb3d
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-16 12:49:48 -0300

    Mudando estratégia para o useState na maior parte da aplicação. Teste1

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,41 @@
 import { useState } from "react";
 function App() {
-  const FirstScreen = ({ nextScreen }) => (
-    <div>
-      <button onClick={nextScreen}>Iniciar Recall!</button>
-    </div>
-  );
+  const [mostrarInicio, setMostrarInicio] = useState(true);
+  const [mostrarFlashCards, setMostrarFlashCard] = useState(false);
+  const [mostrarResultado, setMostrarResultado] = useState(false);
+  const [flashcards, setFlashCars] = useState([
+    {
+      question: "O que é JSX?",
+      answer: "Uma extensão da linguagem JavaScript",
+    },
+    {
+      question: "O React é __",
+      answer: "Uma biblioteca JavaScript para construção de interfaces",
+    },
+    { question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
+    { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+    {
+      question: "O ReactDOM nos ajuda __",
+      answer: "Interagindo com a DOM para colocar componentes React na mesma",
+    },
+    {
+      question: "Usamos o npm para __",
+      answer: "Gerenciar os pacotes necessários e suas dependências",
+    },
+    {
+      question: "Usamos props para __",
+      answer: "Passar diferentes informações para componentes",
+    },
+    {
+      question: "Usamos estado (state) para __",
+      answer:
+        "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+    },
+  ]);
 
-  const SecondScreen = () => (
-    <div>
-      <button> Question </button>
-      <button> Question </button>
-      <button> Question </button>
-      <button> Question </button>
-      <button> Question </button>
-      <button> Question </button>
-      <button> Question </button>
-      <button> Question </button>
-    </div>
-  );
-  const [currentScreen, setCurrentScreen] = useState(false);
-  const nextScreen = () => {
-    setCurrentScreen(true);
+  const startRecall = () => {
+    setMostrarInicio(false);
   };
-
-  return (
-    <div>
-      {currentScreen ? (
-        <SecondScreen />
-      ) : (
-        <FirstScreen nextScreen={nextScreen} />
-      )}
-    </div>
-  );
 }
 
 export default App;
