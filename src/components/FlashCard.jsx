5fac72d0c3bed781c17a654efa96e4f61091db44
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-16 19:39:28 -0300

    colocando os botões

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 const Flashcard = ({ flashcard }) => {
   const [flipped, setFlipped] = useState(false);
+  const [answered, setAnswered] = useState(false);
 
   const handleFlip = () => {
     setFlipped(!flipped);
+  };
+
+  const handleAnswer = (status) => {
+    setAnswered(true);
   };
 
   return (
@@ -16,6 +21,17 @@ const Flashcard = ({ flashcard }) => {
         <div className="card-back">
           <h3>{flashcard.question}</h3>
           <p>{flashcard.answer}</p>
+          {!answered && (
+            <div>
+              <button onClick={() => handleAnswer("Não lembrei")}>
+                Não lembrei
+              </button>
+              <button onClick={() => handleAnswer("Quase não lembrei")}>
+                Quase não lembrei
+              </button>
+              <button onClick={() => handleAnswer("Zap!")}>Zap!</button>
+            </div>
+          )}
         </div>
       </div>
     </div>
