91cf47de5801ad533fabc0a0408e5719a11bee77
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-17 18:38:59 -0300

    add flashcard

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,18 +1,31 @@
 import logo from "../images/logo.png";
+import cards from "../cards";
 import "../css/container.css";
+import Flashcards from "./Flashcards";
+import { useState } from "react";
 
 function Inicio() {
-  //   const [answeredCount, setAnsweredCount] = useState(0);
+  const [answeredCount, setAnsweredCount] = useState(0);
 
-  //   function handleFlashcardCount() {
-  //     setAnsweredCount(answeredCount + 1);
-  //   }
+  function handleFlashcardCount() {
+    setAnsweredCount(answeredCount + 1);
+  }
   return (
     <div className="container">
       <div className="logo">
         <img src={logo} alt="Logo" />
         <h1>ZapRecall</h1>
       </div>
+      {cards.map((card, index) => {
+        return (
+          <Flashcards
+            key={index}
+            index={index}
+            card={card}
+            handleFlashcardCount={handleFlashcardCount}
+          />
+        );
+      })}
     </div>
   );
 }
