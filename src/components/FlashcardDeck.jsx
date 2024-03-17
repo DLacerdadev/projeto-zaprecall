fd602e7baca6c39fd4307e88fbb000691e9216b6
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-16 21:27:49 -0300

    Alteração dos Flashcards serem gerenciados de forma externa

--- src/components/FlashcardDeck.jsx
+++ src/components/FlashcardDeck.jsx
@@ -1,66 +1,25 @@
 import React, { useState } from "react";
 import Flashcard from "./FlashCard";
 
-const FlashcardDeck = () => {
-  const initialFlashcards = [
-    {
-      id: 1,
-      question: "O que é JSX?",
-      answer: "Uma extensão de linguagem do JavaScript",
-    },
-    {
-      id: 2,
-      question: "O React é __?",
-      answer: "Uma biblioteca JavaScript para construção de interfaces",
-    },
-    {
-      id: 3,
-      question: "Componentes devem iniciar com __?",
-      answer: "Letra maiúscula",
-    },
-    {
-      id: 4,
-      question: "Podemos colocar __ dentro do JSX?",
-      answer: "Expressões",
-    },
-    {
-      id: 5,
-      question: "O ReactDOM nos ajuda __?",
-      answer: "Interagindo com a DOM para colocar componentes React na mesma",
-    },
-    {
-      id: 6,
-      question: "Usamos o npm para __?",
-      answer: "Gerenciar os pacotes necessários e suas dependências",
-    },
-    {
-      id: 7,
-      question: "Usamos props para __?",
-      answer: "Passar diferentes informações para componentes",
-    },
-    {
-      id: 8,
-      question: "Usamos estado (state) para __?",
-      answer:
-        "Dizer para o React quais informações, quando atualizadas, devem renderizar a tela novamente",
-    },
-  ];
+const FlashcardDeck = ({ flashcards, onFlashcardAnswer }) => {
+  const [shuffledFlashcards, setShuffledFlashcards] = useState([]);
 
-  const shuffle = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  const shuffleFlashcards = () => {
+    const shuffled = [...flashcards].sort(() => Math.random() - 0.5);
+    setShuffledFlashcards(shuffled);
   };
-
-  const [flashcards, setFlashCards] = useState(shuffle(initialFlashcards));
 
   return (
     <div>
       <h2>Deck de Flashcards</h2>
-      {flashcards.map((flashcard, index) => (
-        <Flashcard key={flashcard.id} flashcard={flashcard} index={index + 1} />
+      <button onClick={shuffleFlashcards}>Iniciar Recall!</button>
+      {shuffledFlashcards.map((flashcard, index) => (
+        <Flashcard
+          key={flashcard.id}
+          flashcard={flashcard}
+          index={index + 1}
+          onFlashcardAnswer={onFlashcardAnswer}
+        />
       ))}
     </div>
   );
