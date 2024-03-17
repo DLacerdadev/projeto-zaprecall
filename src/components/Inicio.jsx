ea01bd114769606a9fda72b4839a2ebc91adac24
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-17 17:37:53 -0300

    fix error initials

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -2,20 +2,19 @@ import logo from "../images/logo.png";
 import "../css/container.css";
 
 function Inicio() {
-  const [answeredCount, setAnsweredCount] = useState(0);
+  //   const [answeredCount, setAnsweredCount] = useState(0);
 
-  function handleFlashcardCount() {
-    setAnsweredCount(answeredCount + 1);
-  }
-}
-
-return (
-  <div className="container">
-    <div className="logo">
-      <img src={logo} alt="Logo" />
-      <h1>ZapRecall</h1>
+  //   function handleFlashcardCount() {
+  //     setAnsweredCount(answeredCount + 1);
+  //   }
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+        <h1>ZapRecall</h1>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Inicio;
