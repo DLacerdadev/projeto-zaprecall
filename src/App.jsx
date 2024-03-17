e4b5663c31da4f379b8a517e5bd4595952085fa0
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-17 17:39:37 -0300

    App start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./css/resetStyle.css";
+import Inicio from "./components/Inicio";
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <Inicio />
+      </div>
+    </div>
+  );
+}
+
+export default App;
