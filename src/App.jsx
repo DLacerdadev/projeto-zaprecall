0f7588c6d514f9c44cc1e76a80267c72231df665
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-16 21:26:36 -0300

    mudança no App para ter os flashcards e primeiros passos do result

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
 import React, { useState } from "react";
 import FlashcardDeck from "./components/FlashcardDeck";
+import Result from "./components/Result";
 
 const App = () => {
-  const [showDeck, setShowDeck] = useState(false);
+  const [answeredCount, setAnsweredCount] = useState(0);
+  const [userResponses, setUserResponses] = useState([]);
 
-  const handleStartRecall = () => {
-    setShowDeck(true);
+  const initialFlashcards = [
+    {
+      id: 1,
+      question: "O que é JSX?",
+      answer: "Uma extensão de linguagem do JavaScript",
+    },
+    {
+      id: 2,
+      question: "O React é __?",
+      answer: "Uma biblioteca JavaScript para construção de interfaces",
+    },
+    {
+      id: 3,
+      question: "Componentes devem iniciar com __?",
+      answer: "Letra maiúscula",
+    },
+    {
+      id: 4,
+      question: "Podemos colocar __ dentro do JSX",
+      answer: "Expressões",
+    },
+    {
+      id: 5,
+      question: "O ReactDOM nos ajuda __?",
+      answer: "Interagindo com a DOM para colocar componentes React na mesma",
+    },
+    {
+      id: 6,
+      question: "Usamos o npm para __?",
+      answer: "Gerenciar os pacotes necessários e suas dependências",
+    },
+    {
+      id: 7,
+      question: "Usamos props para __?",
+      answer: "Passar diferentes informações para componentes",
+    },
+    {
+      id: 8,
+      question: "Usamos estado (state) para __?",
+      answer:
+        "Dizer para o React quais informações, quando atualizadas, devem renderizar a tela novamente",
+    },
+  ];
+  const [flashcards, setFlashCards] = useState(initialFlashcards);
+  const handleFlashcardAnswer = (status) => {
+    setUserResponses([...userResponses, status]);
+    setAnsweredCount(answeredCount + 1);
   };
-
   return (
-    <div>
-      <h1>Bem-vindo ao ZapRecall!</h1>
-      {!showDeck && (
-        <button onClick={handleStartRecall}>Iniciar Recall!</button>
-      )}
-      {showDeck && <FlashcardDeck />}
+    <div className="app">
+      <FlashcardDeck
+        flashcards={flashcards}
+        onFlashcardAnswer={handleFlashcardAnswer}
+      />
+      <Result userResponses={userResponses} answeredCount={answeredCount} />
     </div>
   );
 };
